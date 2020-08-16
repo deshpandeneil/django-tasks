936e95b3164cec80666371c46c9514c9dd8aa67c
Neil Deshpande <67973534+deshpandeneil@users.noreply.github.com>
2020-08-16 14:52:40 +0530

    Added validation for email. Optimized the validation code. Improved general readability of the code.

--- learnDjango/task_1/views.py
+++ learnDjango/task_1/views.py
@@ -10,12 +10,17 @@ def index(request):
 
 
 def result(request):
+
     start = request.POST.get('start')
     end = request.POST.get('end')
+
     if (start > end):
         return render(request, 'task_1/error.html')
+
     numList = []
+
     for i in range(int(start), int(end) + 1):
         numList.append(i)
+
     params = {'result': numList}
     return render(request, 'task_1/result.html', params)
--- learnDjango/task_2/views.py
+++ learnDjango/task_2/views.py
@@ -8,45 +8,66 @@ from .models import Profile
 
 def index(request):
     if request.method == 'POST':
+
         password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
+
         if password1 != password2:
             messages.error(request, 'Passwords don\'t match.')
             return render(request, 'task_2/signUp.html')
+
+        email = request.POST.get('email')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'This email is already associated with another username.')
+            return render(request, 'task_2/signUp.html')
+
         username = request.POST.get('username')
-        users = User.objects.filter(username=username)
-        if len(users) != 0:
+
+        if User.objects.filter(username=username).exists():
             messages.error(request, 'This username already exists. Try another username.')
             return render(request, 'task_2/signUp.html')
-        phone = str(request.POST.get('phone'))
-        if len(phone) != 10:
+
+        phone = request.POST.get('phone')
+
+        if len(str(phone)) != 10:
             messages.error(request, 'Please enter valid phone number.')
             return render(request, 'task_2/signUp.html')
+
         fname = request.POST.get('fname')
         lname = request.POST.get('lname')
-        email = request.POST.get('email')
         gender = request.POST.get('gender')
+
         newUser = User.objects.create_user(username=username, email=email, first_name=fname, last_name=lname,
                                            password=password1)
+
         profile = Profile(user=newUser, phone=phone, gender=gender)
+
         profile.save()
         newUser.save()
+
         messages.success(request, 'Your account has been created.')
+
         return redirect('login')
+
     return render(request, 'task_2/signUp.html')
 
 
 def login(request):
     if request.method == 'POST':
+
         username = request.POST.get('username')
         password = request.POST.get('password')
+
         user = auth.authenticate(request, username=username, password=password)
+
         if user is not None:
             auth.login(request, user)
             return redirect('user-home')
         else:
             messages.error(request, 'Invalid credentials.')
             return redirect('login')
+
     return render(request, 'task_2/login.html')
 
 
@@ -61,15 +82,20 @@ def logout(request):
 
 def search(request):
     if request.method == 'POST':
+
         email = request.POST.get('email')
+
         if email == '':
             messages.error(request, 'Please enter an email address to search.')
             return redirect('search')
+
         user = User.objects.filter(email=email)
+
         if len(user) == 0:
             messages.info(request, 'No user found with the entered email address.')
             return redirect('search')
         else:
             params = {'user': user}
             return render(request, 'task_2/result.html', params)
+
     return render(request, 'task_2/search.html')
